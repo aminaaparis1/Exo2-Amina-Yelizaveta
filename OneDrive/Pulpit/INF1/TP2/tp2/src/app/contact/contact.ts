90c9d4797e2bd4f073343fefee1529851cec81c7
Liza-maker <lizasamujlova01@gmail.com>
2025-09-17 16:54:03 +0200

    Ajout de formulaire

--- OneDrive/Pulpit/INF1/TP2/tp2/src/app/contact/contact.ts
+++ OneDrive/Pulpit/INF1/TP2/tp2/src/app/contact/contact.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  imports:[ReactiveFormsModule, CommonModule],
+  styleUrls: ['./contact.scss']
 })
 export class Contact {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      prenom: ['', Validators.required],
+      nom: ['', Validators.required],
+      age: [''],
+      hideEmail: [false],
+      email: ['', [Validators.required, Validators.email]],
+      commentaire: ['', Validators.required]
+    });
+
+    this.contactForm.get('hideEmail')?.valueChanges.subscribe(val => {
+      const emailCtrl = this.contactForm.get('email');
+      if (val) {
+        emailCtrl?.clearValidators();
+        emailCtrl?.setValue('');
+      } else {
+        emailCtrl?.setValidators([Validators.required, Validators.email]);
+      }
+      emailCtrl?.updateValueAndValidity();
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      alert('Le formulaire est valide');
+      //apres ici ajouter la redirection vers accueil
+    }
+  }
+
 
 }
